1a33e910a7b0225c2299f4c3f9c4a52b38ce3801
Nikita Mohylov <D1353L@users.noreply.github.com>
2020-05-03 01:15:34 +0300

    Mobile design for sign_in page

--- pages/sign_in.vue
+++ pages/sign_in.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="sign-in">
-    <div class="container">
-      <div class="circles-container">
+    <div class="sign-in-container">
+      <div class="circles-container circles-container-top">
         <div class="circle"></div>
         <div class="circle"></div>
       </div>
@@ -38,18 +38,20 @@ export default {}
 </script>
 
 <style scoped lang="scss">
+$container-width: 433px;
+
 .sign-in {
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
 
-  .container {
-    width: 433px;
+  .sign-in-container {
+    width: $container-width;
     height: 227px;
     background: rgba(0, 0, 0, 0.623);
     text-align: center;
-    padding: 6px;
+    position: relative;
 
     p {
       font-weight: 300;
@@ -59,70 +61,123 @@ export default {}
     }
 
     .circles-container {
+      position: absolute;
       display: flex;
       justify-content: space-between;
       width: 100%;
+      padding: 10px;
+      left: 0;
 
       .circle {
-        width: 9px;
-        height: 9px;
-        border-radius: 100px;
+        width: 10px;
+        height: 10px;
+        border-radius: 100%;
         background: $light-gray;
       }
     }
 
+    .circles-container-top {
+      top: 0;
+      padding-bottom: 0;
+    }
+
     .circles-container-bottom {
-      margin-top: 0.3em;
+      bottom: 0;
+      padding-top: 0;
     }
 
     .title {
       font-weight: 100;
       font-size: $font-medium;
       color: $white;
-      margin-top: 0.2em;
+      margin-top: 0.8em;
     }
 
     .social-media-list {
-      width: 86%;
-      margin: 2.16em auto 2em;
+      width: 83.5%;
+      margin: 2.1em auto 2em;
       display: flex;
 
       a {
         flex-grow: 1;
         font-size: $font-large;
         color: $white;
-
-        [data-icon='google'] {
-          margin-top: 0.083em;
-        }
-        [data-icon='facebook-f'] {
-          margin-top: 0.06em;
-        }
-        [data-icon='twitter'] {
-          margin-top: 0.15627em;
-          margin-left: -6%;
-        }
-        [data-icon='linkedin-in'] {
-          margin-top: 0.18em;
-        }
-        [data-icon='github'] {
-          margin-top: 0.098em;
-        }
-      }
-
-      a + a {
-        margin-left: 9%;
       }
     }
   }
 }
 
-/* Media requests */
-$breakpoint-tablet: 460px;
-@media (max-width: $breakpoint-tablet) {
+@media (min-width: $container-width) {
+  [data-icon='google'] {
+    margin-top: 0.083em;
+  }
+  [data-icon='facebook-f'] {
+    margin-top: 0.06em;
+  }
+  [data-icon='twitter'] {
+    margin-top: 0.15627em;
+    margin-left: -6%;
+  }
+  [data-icon='linkedin-in'] {
+    margin-top: 0.18em;
+  }
+  [data-icon='github'] {
+    margin-top: 0.098em;
+  }
+
+  a + a {
+    margin-left: 9%;
+  }
+}
+
+@media (max-width: $container-width) {
   .sign-in {
-    .container {
-      width: 90%;
+    width: 100vw;
+    height: 100vh;
+
+    .sign-in-container {
+      width: 100%;
+      height: 100%;
+      padding: 10px;
+
+      .title {
+        margin-top: 1.15em;
+        font-size: 7vw;
+      }
+
+      .social-media-list {
+        flex-direction: column;
+        height: 70%;
+        margin: 2.6em auto -0.3em auto;
+
+        a {
+          font-size: 8vh;
+        }
+
+        [data-icon='facebook-f'] {
+          margin-top: 0.05em;
+          margin-left: 0.05em;
+        }
+        [data-icon='twitter'] {
+          margin-top: 0.05em;
+        }
+        [data-icon='linkedin-in'] {
+          margin-top: -0.05em;
+          margin-bottom: -0.1em;
+        }
+        [data-icon='github'] {
+          margin-bottom: 0.1em;
+        }
+      }
+
+      p {
+        margin-bottom: 0.2em;
+        font-size: 3.33vw;
+      }
+
+      .circles-container-bottom {
+        margin-top: 2.8em;
+      }
     }
   }
 }
